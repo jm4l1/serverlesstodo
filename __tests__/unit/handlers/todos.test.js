e708a6abaa18e5646384cb62c5eae94f7a7cb7b9
Jamal Moore <j_mali26@hotmail.com>
2020-09-03 15:16:16 -0400

    - added test file

---
+++ __tests__/unit/handlers/todos.test.js
@@ -0,0 +1,7 @@
+
+const lambda = require('../../../src/handlers/index.js');
+
+describe('Test for hello-from-lambda', function () {
+    it('Verifies successful response', async () => {
+    });
+});
